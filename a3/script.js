17c716d06cd7701a0a2993712bcab9024f9c89e3
Ashton Gibson <ashton.gibson@bellevuecollege.edu>
2025-05-25 01:28:18 -0700

    Push for compliance - again

--- a3/script.js
+++ a3/script.js
@@ -1,18 +1,57 @@
 var rhombusMain = document.getElementById('rhombus');
+var height = document.getElementById('height').value;
+/* 
+For whatever reason the height must be declared in JS. Otherwise,
+xxx.value outputs the entire element itself. None of the other inputs
+requre it. Debugging that one was a pain, and I don't fully understand
+why it happened at the moment
+*/
 
 function createRhombus(height, evenColor, oddColor, symbol) {
-    rhombusMain.innerHTML = "<p>3</p>";
-    for (var i = 0; i <= height; i++) {
-        for (var j = 0; j <= height; j++) {
-            if (j <= i) {
-                rhombusMain.innerHTML += "*";
-            }
-            else
-            {
-                rhombusMain.innerHTML += "_";
-            }
-        }
-
-        rhombusMain.innerHTML += "<br>";
-    }
+    createTopLayer(height);
+    createBottomLayer(height);
 };
+
+function createTopLayer(currentY) {
+    var padding = height - currentY;
+    var newLine = "<br>";
+
+    for (var i = 0; i < padding; i++)
+    {
+        newLine += "_";
+    }
+
+    for (var i = 0; i < currentY * 2; i++)
+    {
+        newLine += "*";
+    }
+
+    rhombusMain.innerHTML = newLine + rhombusMain.innerHTML;
+
+    if (currentY > 1)
+    {
+        createTopLayer(currentY - 1)
+    }
+}
+
+function createBottomLayer(currentY) {
+    var padding = height - currentY;
+    var newLine = "<br>";
+
+    for (var i = 0; i < padding; i++)
+    {
+        newLine += "_";
+    }
+
+    for (var i = 0; i < currentY * 2; i++)
+    {
+        newLine += "*";
+    }
+
+    rhombusMain.innerHTML = newLine + rhombusMain.innerHTML;
+
+    if (currentY > 1)
+    {
+        createTopLayer(currentY - 1)
+    }
+}
